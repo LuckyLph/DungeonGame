4014f2ebbe034feed6e9848a1ce0492f58d41179
Louis-Philippe Hudon <lp.hudon@hotmail.com>
2021-06-13 16:48:28 -0400

    bot stuff!

--- Assets/Scripts/Gameplay/Mobs/AIController.cs
+++ Assets/Scripts/Gameplay/Mobs/AIController.cs
@@ -1,20 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
-namespace Pathfinding
+public class AIController : MonoBehaviour
 {
-    public class AIController : AIBase
+    [SerializeField]
+    private bool agentEnabled;
+
+    public bool AgentEnabled
     {
+        get { return agentEnabled; }
+        set { agentEnabled = value; }
+    }
 
-        protected override void Update()
+    private AIPath agent;
+    private bool facingRight = false;
+    private Vector3 currentMovementVector;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private bool hasMoved;
+
+    private void Awake()
+    {
+        agent = GetComponent<AIPath>();
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        agent.OnAIMove += OnAIMove;
+    }
+
+    private void OnDisable()
+    {
+        agent.OnAIMove -= OnAIMove;
+    }
+
+    void Update()
+    {
+        agent.IsEnabled = AgentEnabled;
+
+        if (!agent.isStopped)
         {
-
+            hasMoved = true;
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("inputX", currentMovementVector.x);
+            animator.SetFloat("inputY", currentMovementVector.y);
+            //animator.speed = Mathf.Max(Vector3.Distance(Vector3.zero, currentMovementVector), 0.2f);
+            if ((currentMovementVector.x > 0 && facingRight) || (currentMovementVector.x < 0 && !facingRight))
+            {
+                Flip();
+            }
         }
+    }
 
-        protected override void FixedUpdate()
+    private void LateUpdate()
+    {
+        if (!hasMoved)
         {
-
+            animator.SetBool("isWalking", false);
         }
+        hasMoved = false;
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = spriteRenderer.transform.localScale;
+        theScale.x *= -1;
+        spriteRenderer.transform.localScale = theScale;
+    }
+
+    public void OnAIMove(Vector3 movement)
+    {
+        currentMovementVector = movement;
     }
 }
